0cb13f4b95ab380fc9be6ad191aa74fb557f9d1d
Ramona Stanberry <monacstanberry@gmail.com>
2021-10-10 18:40:53 -0500

    Added X or O to square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,8 +5,28 @@ function boardLayout(game_board){
     }
 }
 
-
 onload=function(){
     var gBoard= document.getElementById("board");
-    boardLayout(gBoard);
+    boardLayout(gBoard); 
+    var allSquares=document.querySelectorAll('.square');
+    const p1='X';
+    const p2='O';
+    var array=[];
+    let this_player;
+
+    for(let i=0;i<=8;i++){
+        allSquares[i].addEventListener('click',function(){
+            if(this_player===p1){
+                this_player=p2;
+                document.getElementById('board').getElementsByTagName('div')[i].className+=(' O');
+            }
+            else{
+                this_player=p1;
+                document.getElementById('board').getElementsByTagName('div')[i].className+=(' X');
+            }
+            array.push(allSquares[i].innerHTML=this_player);
+            console.log('clicked')
+        })
+
+    }
 }
