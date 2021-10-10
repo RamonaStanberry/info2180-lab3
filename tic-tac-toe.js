91a004c5375218e94a308fe5f77e0d118d6ba8ee
Ramona Stanberry <monacstanberry@gmail.com>
2021-10-10 17:50:30 -0500

    Layout the board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,12 @@
-onload=function(){
-    const Player1='X';
-    const Player2= 'O';
-    var game_board= document.getElementById("board").getElementsByTagName('div');
-    for(let i=0;i<game_board.length;i++)
+function boardLayout(game_board){
+    for(let i=0;i<game_board.children.length;i++)
     {
-        game_board[i].className+=('square');
+        game_board.children[i].className+=('square');
     }
-    document.getElementById("board").getElementsByTagName('div').className+=('X');
-    document.getElementById("board").getElementsByTagName('div').className+=('O');
+}
+
+
+onload=function(){
+    var gBoard= document.getElementById("board");
+    boardLayout(gBoard);
 }
