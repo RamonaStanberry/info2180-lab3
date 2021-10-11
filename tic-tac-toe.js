6a5cbd3f552cf00938aad2a90b280a51e7bf5a04
Ramona Stanberry <monacstanberry@gmail.com>
2021-10-10 19:16:13 -0500

    Style change when mouse hovers

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,22 +11,31 @@ onload=function(){
     var allSquares=document.querySelectorAll('.square');
     const p1='X';
     const p2='O';
-    var array=[];
-    let this_player;
+    var game_state=[];
+    let this_player= p2;
 
     for(let i=0;i<=8;i++){
         allSquares[i].addEventListener('click',function(){
-            if(this_player===p1){
+            if(this_player===p1 && allSquares[i].textContent===''){
                 this_player=p2;
                 document.getElementById('board').getElementsByTagName('div')[i].className+=(' O');
             }
-            else{
+            else if(this_player===p2 && allSquares[i].textContent==='') {
                 this_player=p1;
                 document.getElementById('board').getElementsByTagName('div')[i].className+=(' X');
             }
-            array.push(allSquares[i].innerHTML=this_player);
-            console.log('clicked')
+            game_state.push(allSquares[i].innerHTML=this_player);
+            //console.log('clicked')
         })
-
+        
+        allSquares[i].addEventListener("mouseover", event => {
+            allSquares[i].className+=(' hover');
+        });
+        allSquares[i].addEventListener("mouseleave", event => {
+            allSquares[i].classList.remove('hover');
+        });
     }
-}
+
+
+
+}   
